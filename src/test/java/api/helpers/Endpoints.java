b8e35098c98dd4deda9a50770c00b6c27adfd178
olsenfromhell <olsenfromhell@gmail.com>
2023-03-22 20:27:38 +0300

    specification is deleted, added global uri and basePath, added soft assertion

--- src/test/java/api/helpers/Endpoints.java
+++ src/test/java/api/helpers/Endpoints.java
@@ -1,9 +1,10 @@
 package api.helpers;
 
 public class Endpoints {
-    public static final String URL = "https://reqres.in";
-    public static final String UserPage = "/api/users?page={id}";
-    public static final String UserRegisterPath = "/api/register";
-    public static final String ListResource = "/api/unknown";
-    public static final String UserWithId = "/api/users/{id}";
+
+    public static final String UserPage = "/users?page={id}";
+    public static final String UserRegisterPath = "/register";
+    public static final String ListResource = "/unknown";
+    public static final String UserWithId = "/users/{id}";
+
 }
--- src/test/java/com/example/project/ReqresTest.java
+++ src/test/java/com/example/project/ReqresTest.java
@@ -1,13 +1,18 @@
 package com.example.project;
 
-import api.data.*;
+import api.data.ColorsData;
+import api.data.UserData;
 import api.helpers.Endpoints;
-import api.registration.*;
-import api.specification.Specification;
+import api.registration.Registration;
+import api.registration.RegistrationSuccess;
+import api.registration.RegistrationUnsuccessful;
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,38 +22,42 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class ReqresTest {
+class Base {
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+    }
+}
+
+public class ReqresTest extends Base {
 
     @Test
     @DisplayName("POJO: User avatar link contains the same id as the user")
     public void checkAvatarAndIdTest() {
-        Specification.installSpecification(
-                Specification.requestSpecification(Endpoints.URL),
-                Specification.responseSpecificationOK200()
-        );
-
         List<UserData> users = given()
-                .when().get(Endpoints.UserPage, 2)
-                .then().log().all()
+                .when().log().all()
+                .get(Endpoints.UserPage, 2)
+                .then()
+                .statusCode(200)
                 .extract().body().jsonPath()
                 .getList("data", UserData.class);
 
-        users.forEach(x -> assertTrue(x.getAvatar().contains(x.getId().toString())));
-        assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")));
+        assertAll(
+            () -> users.forEach(x -> assertTrue(x.getAvatar().contains(x.getId().toString()))),
+            () -> assertTrue(users.stream().allMatch(x -> x.getEmail().endsWith("@reqres.in")))
+        );
     }
 
     @Test
     @DisplayName("No POJO: User avatar link contains the same id as the user")
     public void checkAvatarAndIdTestNoPojo() {
-        Specification.installSpecification(
-                Specification.requestSpecification(Endpoints.URL),
-                Specification.responseSpecificationOK200()
-        );
-
         Response response = given()
-                .when()
+                .when().log().all()
                 .get(Endpoints.UserPage, 2)
-                .then().log().all()
+                .then()
+                .statusCode(200)
                 .body("page", equalTo(2))
                 .body("data.id", notNullValue())
                 .body("data.email", notNullValue())
@@ -72,56 +81,49 @@ public class ReqresTest {
     @Test
     @DisplayName("New user registered successfully")
     public void checkRegistrationSuccessTest() {
-        Specification.installSpecification(
-                Specification.requestSpecification(Endpoints.URL),
-                Specification.responseSpecificationOK200()
-        );
-
         int id = 4;
         String token = "QpwL5tke4Pnpja7X4";
 
         Registration user = new Registration("eve.holt@reqres.in", "pistol");
         RegistrationSuccess registrationSuccess = given()
                 .body(user)
-                .when().post(Endpoints.UserRegisterPath)
-                .then().log().all()
+                .when().log().all()
+                .post(Endpoints.UserRegisterPath)
+                .then()
+                .statusCode(200)
                 .extract().as(RegistrationSuccess.class);
 
-        assertNotNull(registrationSuccess.getId());
-        assertNotNull(registrationSuccess.getToken());
-        assertEquals(id, registrationSuccess.getId());
-        assertEquals(token, registrationSuccess.getToken());
+        assertAll(
+            () -> assertNotNull(registrationSuccess.getId()),
+            () ->assertNotNull(registrationSuccess.getToken()),
+            () ->assertEquals(id, registrationSuccess.getId()),
+            () ->assertEquals(token, registrationSuccess.getToken())
+        );
     }
 
     @Test
     @DisplayName("User unsuccessful registration")
     public void checkUnsuccessfulRegistrationTest() {
-        Specification.installSpecification(
-                Specification.requestSpecification(Endpoints.URL),
-                Specification.responseSpecificationError400()
-        );
-
         Registration user = new Registration("sydney@fife", "");
         RegistrationUnsuccessful registrationUnsuccessful = given()
                 .body(user)
-                .when().post(Endpoints.UserRegisterPath)
-                .then().log().all()
+                .when().log().all()
+                .post(Endpoints.UserRegisterPath)
+                .then()
+                .statusCode(400)
                 .extract().as(RegistrationUnsuccessful.class);
 
-        assertEquals("Missing password", registrationUnsuccessful.getError());
+        assertEquals("Missing email or username", registrationUnsuccessful.getError());
     }
 
     @Test
     @DisplayName("Get Sorted years test")
     public void getSortedYearsTest() {
-        Specification.installSpecification(
-                Specification.requestSpecification(Endpoints.URL),
-                Specification.responseSpecificationOK200()
-        );
-
         List<ColorsData> colors = given()
-                .when().get(Endpoints.ListResource)
-                .then().log().all()
+                .when().log().all()
+                .get(Endpoints.ListResource)
+                .then()
+                .statusCode(200)
                 .extract().body().jsonPath().getList("data", ColorsData.class);
 
         List<Integer> years = colors.stream().map(ColorsData::getYear).collect(Collectors.toList());
@@ -133,22 +135,11 @@ public class ReqresTest {
     @Test
     @DisplayName("Delete user with id: '2'")
     public void deleteUserTest() {
-        Specification.installSpecification(
-                Specification.requestSpecification(Endpoints.URL),
-                Specification.responseSpecificationUniqueStatus(204)
-        );
+
         given()
-                .when().delete(Endpoints.UserWithId, 2)
-                .then().log().all();
+                .when().log().all()
+                .delete(Endpoints.UserWithId, 2)
+                .then()
+                .statusCode(204);
     }
 }
-
-
-
-
-
-
-
-
-
-
